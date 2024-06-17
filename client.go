b48d4dfa2cb5ec7f3d64bab7b990b335de79ec8e
buynaa <buynaaesther@gmail.com>
2024-06-17 12:18:40 +0800

    init

--- client.go
+++ client.go
@@ -1,4 +1,9 @@
+package websocket
 import (
+	"fmt"
+	"log"
+	"sync"
+
 	"github.com/buynaa0314/websocket"
 )
 
@@ -7,4 +12,26 @@ type Client struct {
 	Conn *websocket.Conn
 	Pool *Pool
 	mu   sync.Mutex
+}
+type Message struct{
+	Type int 'json:"type"'
+	Body string 'json:"body"'
+}
+func (c *Client) Read(){
+	defer func(){
+		c.Pool.Unregister<- c
+		c.Conn.Close()
+	}()
+	for{
+	messageType, p, err:=c.Conn.ReadMessage()
+	if err!= nil{
+		log.Println(err)
+		return
+	}
+	message := Message{Type:messageType , Body: string(p)}
+	c.Pool.Broadcast <- message 
+	fmt.Printf("Message received:%+V\n , message")
+}
+
+
 }
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/buynaa0314/goloang_chat
+
+go 1.16
+
+
+require github.com/buynaa0314/websocket v1.5.0
